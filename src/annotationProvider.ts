afe18b3414d34618a250067d535f92ee6e80a0d7
bzrncev <bzrncev@users.noreply.github.com>
2020-05-18 02:27:27 +0200

    Formating

--- src/annotationProvider.ts
+++ src/annotationProvider.ts
@@ -1,20 +1,40 @@
-import { DecorationInstanceRenderOptions, ThemeColor, DecorationOptions, Range, workspace } from "vscode";
+import {
+  DecorationInstanceRenderOptions,
+  ThemeColor,
+  DecorationOptions,
+  Range,
+  workspace,
+} from "vscode";
 
 export class Annotations {
-    public static paramAnnotation(message: string, range: Range): DecorationOptions {
-        return {
-            range,
-            renderOptions: {
-                before: {
-                    color: new ThemeColor("phpannotations.annotationForeground"),
-                    backgroundColor: new ThemeColor("phpannotations.annotationBackground"),
-                    contentText: message,
-                    margin: "0px " + workspace.getConfiguration("phpannotations").get("margin") + "px;padding: 0 3px;",
-                    height: '-10px',
-                    fontStyle: workspace.getConfiguration("phpannotations").get("fontStyle"),
-                    fontWeight: workspace.getConfiguration("phpannotations").get("fontWeight") + ";font-size:" + workspace.getConfiguration("phpannotations").get("fontSize") + "px;"
-                }
-            } as DecorationInstanceRenderOptions
-        } as DecorationOptions;
-    }
+  public static paramAnnotation(
+    message: string,
+    range: Range
+  ): DecorationOptions {
+    return {
+      range,
+      renderOptions: {
+        before: {
+          color: new ThemeColor("phpannotations.annotationForeground"),
+          backgroundColor: new ThemeColor(
+            "phpannotations.annotationBackground"
+          ),
+          contentText: message,
+          margin:
+            "0px " +
+            workspace.getConfiguration("phpannotations").get("margin") +
+            "px;padding: 0 3px;",
+          height: "-10px",
+          fontStyle: workspace
+            .getConfiguration("phpannotations")
+            .get("fontStyle"),
+          fontWeight:
+            workspace.getConfiguration("phpannotations").get("fontWeight") +
+            ";font-size:" +
+            workspace.getConfiguration("phpannotations").get("fontSize") +
+            "px;",
+        },
+      } as DecorationInstanceRenderOptions,
+    } as DecorationOptions;
+  }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -1,115 +1,146 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import Commands from "./commands";
 import * as parser from "./parser";
 import { Annotations } from "./annotationProvider";
 
 const hintDecorationType = vscode.window.createTextEditorDecorationType({});
 
-function getAttrName(editor: vscode.TextEditor, position: vscode.Position, key: number) {
-	return new Promise(async (resolve, reject) => {
-		var args: vscode.Location[] = [];
-		var hoverCommand: any = await vscode.commands.executeCommand<vscode.Hover[]>("vscode.executeHoverProvider", editor.document.uri, position);
+function getAttrName(
+  editor: vscode.TextEditor,
+  position: vscode.Position,
+  key: number
+) {
+  return new Promise(async (resolve, reject) => {
+    var args: vscode.Location[] = [];
+    var hoverCommand: any = await vscode.commands.executeCommand<
+      vscode.Hover[]
+    >("vscode.executeHoverProvider", editor.document.uri, position);
 
-		if (hoverCommand && hoverCommand.length > 0) {
-			try {
-				const regEx = /(?<=@param.+)(\$[a-zA-Z0-9_]+)/g;
-				args = hoverCommand[0].contents[0].value.match(regEx);
-			} catch(err) {}
-		}
-        if (args) {
-            resolve(args[key]);
-        }
+    if (hoverCommand && hoverCommand.length > 0) {
+      try {
+        const regEx = /(?<=@param.+)(\$[a-zA-Z0-9_]+)/g;
+        args = hoverCommand[0].contents[0].value.match(regEx);
+      } catch (err) {}
+    }
+    if (args) {
+      resolve(args[key]);
+    }
 
-        reject();
-	});
+    reject();
+  });
 }
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
-	let timeout: NodeJS.Timer | undefined = undefined;
+  let timeout: NodeJS.Timer | undefined = undefined;
 
-	let activeEditor = vscode.window.activeTextEditor;
-	
-	if (activeEditor && activeEditor.document.languageId !== 'php') {
-		return;
-	}
+  let activeEditor = vscode.window.activeTextEditor;
 
-	async function updateDecorations() {
-		if (!activeEditor) {
-			return;
-        }
-        
-        const isEnabled = vscode.workspace.getConfiguration("phpannotations").get("enabled");
+  if (activeEditor && activeEditor.document.languageId !== "php") {
+    return;
+  }
 
-        if (!isEnabled) {
-            activeEditor.setDecorations(hintDecorationType, []);
-            return;
-        }
-
-		const text = activeEditor.document.getText();
-		let phpArguments = [];
-		try {
-			phpArguments = parser.parse(text);
-		} catch (err) {
-		}
-
-		if (phpArguments.length === 0) {
-			return;
-		}
-
-		const phpFunctions: vscode.DecorationOptions[] = [];
-
-		for (let index = 0; index < phpArguments.length; index++) {
-			var argument = phpArguments[index];
-
-			const start = new vscode.Position(argument.start.line, argument.start.character);
-			const end = new vscode.Position(argument.end.line, argument.end.character);
-
-            let args: any;
-            try {
-                args = await getAttrName(activeEditor, new vscode.Position(argument.expression.line, argument.expression.character), argument.key);
-            } catch(err) { }
-
-            if (args) {
-				const decorationPHP = Annotations.paramAnnotation(args.replace('$', '  ') + ':  ', new vscode.Range(start, end));
-
-				phpFunctions.push(decorationPHP);
-			}
-		}
-
-		activeEditor.setDecorations(hintDecorationType, phpFunctions);
-	}
-
-	function triggerUpdateDecorations(timer: boolean = true) {
-		if (timeout) {
-			clearTimeout(timeout);
-			timeout = undefined;
-		}
-		timeout = setTimeout(updateDecorations, timer ? 2500 : 10);
+  async function updateDecorations() {
+    if (!activeEditor) {
+      return;
     }
-    
-    vscode.workspace.onDidChangeConfiguration((event) => {
-        if (event.affectsConfiguration("phpannotations")) {
-            triggerUpdateDecorations(false);
-        }
-    });
 
-    Commands.registerCommands();
+    const isEnabled = vscode.workspace
+      .getConfiguration("phpannotations")
+      .get("enabled");
 
-	vscode.window.onDidChangeActiveTextEditor(editor => {
-		activeEditor = editor;
-		if (editor) {
-			triggerUpdateDecorations();
-		}
-	}, null, context.subscriptions);
+    if (!isEnabled) {
+      activeEditor.setDecorations(hintDecorationType, []);
+      return;
+    }
 
-	vscode.workspace.onDidChangeTextDocument(event => {
-		if (activeEditor && event.document === activeEditor.document) {
-            triggerUpdateDecorations(false);
-		}
-    }, null, context.subscriptions);
-    
-    if (activeEditor) {
+    const text = activeEditor.document.getText();
+    let phpArguments = [];
+    try {
+      phpArguments = parser.parse(text);
+    } catch (err) {}
+
+    if (phpArguments.length === 0) {
+      return;
+    }
+
+    const phpFunctions: vscode.DecorationOptions[] = [];
+
+    for (let index = 0; index < phpArguments.length; index++) {
+      var argument = phpArguments[index];
+
+      const start = new vscode.Position(
+        argument.start.line,
+        argument.start.character
+      );
+      const end = new vscode.Position(
+        argument.end.line,
+        argument.end.character
+      );
+
+      let args: any;
+      try {
+        args = await getAttrName(
+          activeEditor,
+          new vscode.Position(
+            argument.expression.line,
+            argument.expression.character
+          ),
+          argument.key
+        );
+      } catch (err) {}
+
+      if (args) {
+        const decorationPHP = Annotations.paramAnnotation(
+          args.replace("$", "  ") + ":  ",
+          new vscode.Range(start, end)
+        );
+
+        phpFunctions.push(decorationPHP);
+      }
+    }
+
+    activeEditor.setDecorations(hintDecorationType, phpFunctions);
+  }
+
+  function triggerUpdateDecorations(timer: boolean = true) {
+    if (timeout) {
+      clearTimeout(timeout);
+      timeout = undefined;
+    }
+    timeout = setTimeout(updateDecorations, timer ? 2500 : 10);
+  }
+
+  vscode.workspace.onDidChangeConfiguration((event) => {
+    if (event.affectsConfiguration("phpannotations")) {
+      triggerUpdateDecorations(false);
+    }
+  });
+
+  Commands.registerCommands();
+
+  vscode.window.onDidChangeActiveTextEditor(
+    (editor) => {
+      activeEditor = editor;
+      if (editor) {
         triggerUpdateDecorations();
-    }
+      }
+    },
+    null,
+    context.subscriptions
+  );
+
+  vscode.workspace.onDidChangeTextDocument(
+    (event) => {
+      if (activeEditor && event.document === activeEditor.document) {
+        triggerUpdateDecorations(false);
+      }
+    },
+    null,
+    context.subscriptions
+  );
+
+  if (activeEditor) {
+    triggerUpdateDecorations();
+  }
 }
